4431d3f6cc718cf60f8582f6bfa76a10b5d60786
anorexbill <bryteanorchie1@gmail.com>
2021-02-22 01:57:50 -0700

    Initial Commit

---
+++ SwiftUITutorials/SwiftUITutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialsApp.swift
+//  SwiftUITutorials
+//
+//  Created by Frimpong Anorchie II on 22/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
